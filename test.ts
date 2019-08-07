22465b798002625f8a987a4af4563e38728369b9
Antoni Kepinski <xxczaki@pm.me>
2019-08-07 23:27:20 +0200

    Add different tests for usage without `Cashify` constructor

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 import test from 'ava';
-import Cashify from './dist';
+import {Cashify, convert} from './dist';
 
 const rates = {
 	GBP: 0.92,
@@ -35,6 +35,40 @@ test('`rates` without `base` currency', t => {
 test('rates do not contain `to`', t => {
 	const error = t.throws(() => {
 		cashify.convert(10, {from: 'EUR', to: 'PHP'});
+	}, Error);
+
+	t.is(error.message, '`rates` do not contain `to` currency!');
+});
+
+test('basic conversion (without constructor)', t => {
+	t.is(convert(12, {from: 'USD', to: 'GBP', base: 'EUR', rates}), 9.857142857142858);
+});
+
+test('`from` equals `base` (without constructor)', t => {
+	t.is(convert(10, {from: 'EUR', to: 'GBP', base: 'EUR', rates}), 9.200000000000001);
+});
+
+test('`to` equals `base` (without constructor)', t => {
+	t.is(convert(10, {from: 'GBP', to: 'EUR', base: 'EUR', rates}), 10.869565217391305);
+});
+
+test('`rates` without `base` currency (without constructor)', t => {
+	const rates = {
+		GBP: 0.92,
+		USD: 1.12
+	};
+
+	t.is(convert(10, {from: 'EUR', to: 'GBP', base: 'EUR', rates}), 9.200000000000001);
+});
+
+test('rates do not contain `to` (without constructor)', t => {
+	const error = t.throws(() => {
+		convert(10, {
+			from: 'EUR',
+			to: 'PHP',
+			base: 'EUR',
+			rates
+		});
 	}, Error);
 
 	t.is(error.message, '`rates` do not contain `to` currency!');
