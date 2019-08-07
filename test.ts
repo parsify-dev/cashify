400090a754c332bea8cee27f4c4bd73d564cb4ec
Antoni Kepinski <xxczaki@pm.me>
2019-08-07 23:41:43 +0200

    Additional tests

--- test.ts
+++ test.ts
@@ -21,6 +21,16 @@ test('`to` equals `base`', t => {
 	t.is(cashify.convert(10, {from: 'GBP', to: 'EUR'}), 10.869565217391305);
 });
 
+test('`from` equals `to`', t => {
+	t.is(cashify.convert(10, {from: 'EUR', to: 'EUR'}), 10);
+});
+
+test('`from` equals `to`, but `base` is different', t => {
+	const cashify = new Cashify({base: 'USD', rates});
+
+	t.is(cashify.convert(10, {from: 'EUR', to: 'EUR'}), 10);
+});
+
 test('`rates` without `base` currency', t => {
 	const rates = {
 		GBP: 0.92,
@@ -50,6 +60,14 @@ test('`from` equals `base` (without constructor)', t => {
 
 test('`to` equals `base` (without constructor)', t => {
 	t.is(convert(10, {from: 'GBP', to: 'EUR', base: 'EUR', rates}), 10.869565217391305);
+});
+
+test('`from` equals `to` (without constructor)', t => {
+	t.is(convert(10, {from: 'EUR', to: 'EUR', base: 'EUR', rates}), 10);
+});
+
+test('`from` equals `to`, but `base` is different (without constructor)', t => {
+	t.is(convert(10, {from: 'EUR', to: 'EUR', base: 'USD', rates}), 10);
 });
 
 test('`rates` without `base` currency (without constructor)', t => {
