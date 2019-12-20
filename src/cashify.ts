1b3682f0032fe9a9b25a5a2429f9ed624717d281
Richie Bendall <richiebendall@gmail.com>
2019-12-20 16:22:55 +1300

    chore: options Partials
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- src/cashify.ts
+++ src/cashify.ts
@@ -2,14 +2,14 @@ import {Options} from './lib/options';
 import convert from './convert';
 
 export default class Cashify {
-	constructor(public readonly options: Options) { }
+	constructor(public readonly options: Partial<Options>) { }
 
 	/**
 	* @param amount Amount of money you want to convert.
 	* @param options Conversion options.
 	* @return Conversion result.
 	*/
-	convert(amount: number, options?: Options): number {
-		return convert(amount, {...this.options, ...options});
+	convert(amount: number, options?: Partial<Options>): number {
+		return convert(amount, {...this.options, ...options} as Options);
 	}
 }
