93101127a9d1d675bde6fde71405d25c51205439
Richie Bendall <richiebendall@gmail.com>
2019-12-20 16:00:09 +1300

    feat: Cashify class accept any options
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- src/cashify.ts
+++ src/cashify.ts
@@ -1,16 +1,15 @@
-import {Except} from 'type-fest';
 import Options from './lib/options';
 import convert from './convert';
 
 export default class Cashify {
-	constructor(public readonly options: Except<Options, 'from' | 'to'>) { }
+	constructor(public readonly options: Options) { }
 
 	/**
 	* @param amount Amount of money you want to convert.
 	* @param options Conversion options.
 	* @return Conversion result.
 	*/
-	convert(amount: number, options: Except<Options, 'base' | 'rates'>): number {
+	convert(amount: number, options?: Options): number {
 		return convert(amount, {...this.options, ...options});
 	}
 }
