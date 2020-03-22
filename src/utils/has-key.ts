62533539bd03c39263ab5904df93797e91fc33cb
Antoni Kepinski <xxczaki@pm.me>
2020-03-22 11:57:11 +0100

    Improve type safety

--- src/utils/has-key.ts
+++ src/utils/has-key.ts
@@ -1,8 +1,10 @@
+import {Rates} from '../lib/options';
+
 /**
  * Check if an object contains a key.
  * @param obj The object to check.
  * @param key The key to check for.
 */
-export default function hasKey<T>(obj: T, key: string | number | symbol): key is keyof T {
-	return key in obj;
+export default function hasKey<T>(object: Rates, key: string | number | symbol): key is keyof T {
+	return key in object;
 }
