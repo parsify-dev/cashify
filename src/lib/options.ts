269843a4ed9b4f9ce92ff011e32b1ad58c5a6c77
Antoni Kepinski <antoni@kepinski.ch>
2021-12-05 19:06:26 +0100

    Add `BigJs` option

--- src/lib/options.ts
+++ src/lib/options.ts
@@ -1,3 +1,5 @@
+import type {BigSource, Big} from 'big.js';
+
 export type Rates = Record<string, number>;
 
 export interface Options {
@@ -20,4 +22,9 @@ export interface Options {
 	 * Object containing currency rates (for example from an API, such as Open Exchange Rates).
 	*/
 	rates: Rates;
+
+	/**
+	 * Optional, Big.js constructor - useful to avoid floating point errors.
+	*/
+	BigJs?: new (value: BigSource) => Big;
 }
