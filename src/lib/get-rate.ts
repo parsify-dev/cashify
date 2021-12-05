aa47bba4df9001a8be8fd2b326cfd4f069e7c8a7
Antoni Kepinski <antoni@kepinski.ch>
2021-12-05 19:06:11 +0100

    Improve type safety and make the error messages more succinct

--- src/lib/get-rate.ts
+++ src/lib/get-rate.ts
@@ -1,5 +1,5 @@
-import hasKey from '../utils/has-key';
-import {Rates} from './options';
+import hasKey from '../utils/has-key.js';
+import {Rates} from './options.js';
 
 /**
  * Get the conversion rate.
@@ -18,21 +18,21 @@ export default function getRate(base: string, rates: Rates, from: string | undef
 
 		// If `from` equals `base`, return the basic exchange rate for the `to` currency
 		if (from === base && hasKey(rates, to)) {
-			return rates[to];
+			return rates[to]!;
 		}
 
 		// If `to` equals `base`, return the basic inverse rate of the `from` currency
 		if (to === base && hasKey(rates, from)) {
-			return 1 / rates[from];
+			return 1 / rates[from]!;
 		}
 
 		// Otherwise, return the `to` rate multipled by the inverse of the `from` rate to get the relative exchange rate between the two currencies.
 		if (hasKey(rates, from) && hasKey(rates, to)) {
-			return rates[to] * (1 / rates[from]);
+			return rates[to]! * (1 / rates[from]!);
 		}
 
-		throw new Error('`rates` object does not contain either `from` or `to` currency!');
+		throw new Error('The `rates` object does not contain either the `from` or `to` currency.');
 	} else {
-		throw new Error('Please specify the `from` and/or `to` currency or use parsing!');
+		throw new Error('Please specify the `from` and/or `to` currency, or use parsing.');
 	}
 }
