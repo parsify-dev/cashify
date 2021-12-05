ced389615e651ee80ac36682d11b692b51e91880
Antoni Kepinski <antoni@kepinski.ch>
2021-12-05 19:05:41 +0100

    Add big.js integration

--- src/convert.ts
+++ src/convert.ts
@@ -1,12 +1,13 @@
-import getRate from './lib/get-rate';
-import {Options} from './lib/options';
-import parse from './utils/parser';
+import getRate from './lib/get-rate.js';
+import {Options} from './lib/options.js';
+import parse from './utils/parser.js';
 
 /**
  * Function, which converts currencies based on provided rates.
  *
  * @param {number | string} amount - Amount of money you want to convert.
  * @param {Object} options - Conversion options.
+ * @param {new (value: BigSource) => Big} fn - Optional, Big.js constructor - useful to avoid floating point errors.
  * @return {number} Conversion result.
  *
  * @example
@@ -18,12 +19,21 @@ import parse from './utils/parser';
  *
  * convert(10, {from: 'EUR', to: 'GBP', base: 'EUR', rates}); //=> 9.2
  */
-export default function convert(amount: number | string, {from, to, base, rates}: Options): number {
+export default function convert(
+	amount: number | string,
+	{from, to, base, rates, BigJs}: Options,
+): number {
 	// If provided `amount` is a string, use parsing
 	if (typeof amount === 'string') {
 		const data = parse(amount);
 
-		return (data.amount * 100) * getRate(base, rates, data.from ?? from, data.to ?? to) / 100;
+		amount = data.amount;
+		from = data.from ?? from;
+		to = data.to ?? to;
+	}
+
+	if (BigJs) {
+		return new BigJs(amount).times(getRate(base, rates, from, to)).toNumber();
 	}
 
 	return (amount * 100) * getRate(base, rates, from, to) / 100;
