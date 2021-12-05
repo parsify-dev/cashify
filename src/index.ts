c188658c2a1f11ae3fdaa354647b2d35770b2bd9
Antoni Kepinski <antoni@kepinski.ch>
2021-12-05 19:05:48 +0100

    Lint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-'use strict';
-
-import Cashify from './cashify';
-import convert from './convert';
-import parse from './utils/parser';
+import Cashify from './cashify.js';
+import convert from './convert.js';
+import parse from './utils/parser.js';
 
 export {Cashify, convert, parse};
