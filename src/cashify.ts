44da68d3e44b8bb53d0c805ef0682f94407ec887
Antoni Kepinski <antoni@kepinski.ch>
2021-12-05 19:05:29 +0100

    Greatly simplify the way `Cashify` constructor works

--- src/cashify.ts
+++ src/cashify.ts
@@ -1,6 +1,5 @@
-import {Options} from './lib/options';
-import convert from './convert';
-import parse from './utils/parser';
+import {Options} from './lib/options.js';
+import convert from './convert.js';
 
 export default class Cashify {
 	/**
@@ -28,13 +27,6 @@ export default class Cashify {
 	* cashify.convert(10, {from: 'EUR', to: 'GBP'}); //=> 9.2
 	*/
 	convert(amount: number | string, options?: Partial<Options>): number {
-		// If provided `amount` is a string, use parsing
-		if (typeof amount === 'string') {
-			const data = parse(amount);
-
-			return convert(data.amount, {...this.options, from: data.from, to: data.to, ...options} as Options);
-		}
-
 		return convert(amount, {...this.options, ...options} as Options);
 	}
 }
